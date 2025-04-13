10f06714a402c64b83d3234e221ff9235b3d17eb
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2025-04-13 15:27:58 -0300

    feat: initial project structure

---
+++ cruise_booking_app/README.md
@@ -0,0 +1,11 @@
+## Running RabbitMQ
+
+```bash
+docker compose up -d
+```
+
+## Accessing UI via browser
+
+```
+http://localhost:15672
+```
---
+++ tests/rabbitmq/send.py
@@ -0,0 +1,20 @@
+import pika
+
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host="localhost", credentials=pika.PlainCredentials("admin", "admin")
+        )
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue="hello")
+
+    channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
